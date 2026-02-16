8e18ac893debe80f72822106965bd78acb3be4cb
posapiano-ops <rfiorito@outlook.it>
2026-02-16 11:39:18 +0100

    priorità V1

--- src/assets/TemplateData.ts
+++ src/assets/TemplateData.ts
@@ -1,102 +1,102 @@
 export const templateItems = [
   {
-    group: "Cosa Vuoi fare in ZoOes?",
+    group: "NIS2",
     label: 'Verifica ed implementazione Backup',
-    weight: 1,
+    weight: 2,
     order: 0
   },
   {
-    group: "Cosa Vuoi fare in ZoOes?",
+    group: "NIS2",
     label: 'Ambiente sviluppo e test CNA2',
     weight: 1,
     order: 1
   },
   {
-    group: "Cosa Vuoi fare in ZoOes?",
+    group: "NIS2",
     label: 'Ambiente Sviluppo e test Urbanet',
     weight: 1,
     order: 2
   },
   {
-    group: "Cosa Vuoi fare in ZoOes?",
+    group: "NIS2",
     label: 'Verifica ed implementazione Monitoring, log, XDR e SIEM',
     weight: 1,
     order: 3
   },
   {
-    group: "Cosa Vuoi fare in ZoOes?",
+    group: "NIS2",
     label: 'Migrazione MongoDB a Filesystem',
-    weight: 1,
+    weight: 4,
     order: 4
   },
   {
-    group: "Cosa Vuoi fare in ZoOes?",
+    group: "NIS2",
     label: 'Conf Firewall CISCO',
     weight: 1,
     order: 5
   },
   {
-    group: "Cosa Vuoi fare in ZoOes?",
+    group: "NIS2",
     label: 'Assets Management GRC',
     weight: 1,
     order: 6
   },
   {
-    group: "Cosa Vuoi fare in ZoOes?",
+    group: "NIS2",
     label: 'Update OS obsoleti',
-    weight: 1,
+    weight: 2,
     order: 7
   },
   {
-    group: "Cosa Vuoi fare in ZoOes?",
+    group: "NIS2",
     label: 'Migrazione Repository',
     weight: 1,
     order: 8
   },
   {
-    group: "Cosa Vuoi fare in ZoOes?",
+    group: "NIS2",
     label: 'Migrazione Redmine',
     weight: 1,
     order: 9
   },
   {
-    group: "Cosa Vuoi fare in ZoOes?",
+    group: "NIS2",
     label: 'Migrazione GlusterFS',
     weight: 1,
     order: 10
   },
   {
-    group: "Cosa Vuoi fare in ZoOes?",
+    group: "NIS2",
     label: 'Migrazione Minuorki',
     weight: 1,
     order: 11
   },
   {
-    group: "Cosa Vuoi fare in ZoOes?",
+    group: "NIS2",
     label: 'Migrazione Docker Swarm',
     weight: 1,
     order: 12
   },
   {
-    group: "Cosa Vuoi fare in ZoOes?",
+    group: "NIS2",
     label: 'Active Directory Telese',
     weight: 1,
     order: 13
   },
   {
-    group: "Cosa Vuoi fare in ZoOes?",
+    group: "NIS2",
     label: 'WAF',
     weight: 1,
     order: 14
   },
   {
-    group: "Cosa Vuoi fare in ZoOes?",
+    group: "NIS2",
     label: 'Autenticazione A2F',
     weight: 1,
     order: 15
   },
   {
-    group: "Cosa Vuoi fare in ZoOes?",
+    group: "NIS2",
     label: 'Documentazione API',
     weight: 1,
     order: 16
